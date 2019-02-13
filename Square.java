a217f0428b7f0f3fc326e8b17c714c96c68eb0fe
rleongPd5 <rleong00@stuy.edu>
2019-02-13 05:57:38 -0500

    Added fields and constructor

--- Square.java
+++ Square.java
@@ -1,3 +1,10 @@
 public class Square{
+  //has 3 fields: x coordinate, y coordinate, number of possible moves
+  private int x, y, moves;
+  public Square(int xcor, int ycor, int numMoves){
+    x = xcor;
+    y = ycor;
+    moves = numMoves;
+  }
   
 }
