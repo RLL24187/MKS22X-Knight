df7837586ab8325d8edff63e96bd6f006927e098
rleongPd5 <rleong00@stuy.edu>
2019-02-13 11:40:40 -0500

    Added test case for toString

---
+++ Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+  public static void main(String[] args){
+    KnightBoard k5x5 = new KnightBoard(5,5);
+    System.out.println("-----------Testing KnightBoard constructor-----------\n");
+    System.out.println("-----------Testing toString-----------\n");
+    System.out.println("k5x5: \n");
+    System.out.println(k5x5.toString());
+    System.out.println();
+  }
+}
