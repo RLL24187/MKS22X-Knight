766aa34508a3238fc28efaeddc96fc6cfd32c56d
rleongPd5 <rleong00@stuy.edu>
2019-02-14 11:36:40 -0500

    Worked on toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,14 +1,14 @@
 public class KnightBoard{
   //KnightBoard has 3 public methods and a constructor, a private helper is needed as well.
   //Fields
-  //private boardSquares = Square[][];
+  private boardSquares = Square[][];
   private int[][] board;
   private int rows;
   private int cols;
 
   //@throws IllegalArgumentException when either parameter is negative.
   public KnightBoard(int startingRows,int startingCols){
-    //boardSquares = new Square[startingRows][startingCols];
+    boardSquares = new Square[startingRows][startingCols];
     board = new int[startingRows][startingCols];
     rows = startingRows;
     cols = startingCols;
@@ -16,6 +16,11 @@ public class KnightBoard{
 
     //Initialize the board to the correct size and make them all 0's
 
+/**
+*-you get a blank board if you never called solve or when there is no solution
+*-blank boards display 0's as underscores
+*@return the properly formatted string (see format for toString later in the post)
+*/
 public String toString(){
   String output = "";
   for (int i = 0; i < board.length; i++){
@@ -23,8 +28,11 @@ public String toString(){
       if (board[i][j]>= 10){
         output+= board[i][j];
       }
+      else if (allZero()){
+        output += " _";
+      }
       else{
-        output+= " "+board[i][j];
+        output += " "+board[i][j];
       }
       if (j!= board.length - 1){
         output+=" ";
@@ -83,6 +91,8 @@ public String toString(){
   /**
   *@throws IllegalStateException when the board contains non-zero values.
   *@throws IllegalArgumentException when either parameter is negative or out of bounds.
+  Modifies the board by labeling the moves from 1 (at startingRow,startingCol) up to the area of the board in proper knight move steps.
+  @return true when the board is solvable from the specified starting position
  */
 public boolean solve(int startingRow, int startingCol){
   if (!allZero()){
@@ -91,12 +101,13 @@ public boolean solve(int startingRow, int startingCol){
   if (startingRow < 0 || startingCol <0 || startingRow >= rows || startingCol >= cols){
     throw new IllegalArgumentException("Both parameters must be nonnegative and in the range!");
   }
-  return false;
+  return solveH(startingRow, startingCol, 1);
 }
 
   /**
   *@throws IllegalStateException when the board contains non-zero values.
   *@throws IllegalArgumentException when either parameter is negative or out of bounds.
+  *@return number of solutions when either parameter is negative
   */
 public int countSolutions(int startingRow, int startingCol){
   if (!allZero()){
@@ -108,8 +119,20 @@ public int countSolutions(int startingRow, int startingCol){
   return 0;
 }
 
+private boolean addKnight(int r, int c, int level){ //add a knight
+  //level goes into the int[][] board
+  //reduce the possible moves in boardSquares
+}
+
 //Suggestion:
+//row is starting xcor
+//col is starting ycor
+//level is the number to place in the box
 private boolean solveH(int row ,int col, int level){
+  //Base case:
+  if (level == row * col){
+    return true;
+  }
   return false;
 }
 // level is the # of the knight
