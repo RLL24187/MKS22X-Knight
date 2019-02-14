2ff3234593082a2f01a1e13cadea461a9d955053
rleongPd5 <rleong00@stuy.edu>
2019-02-14 05:03:57 -0500

    Fixed up test cases

--- Driver.java
+++ Driver.java
@@ -10,9 +10,16 @@ public class Driver{
     System.out.println("-----------Testing solve() exceptions-----------\n");
     System.out.println("k5x5(5, 0) --> IllegalArgumentException\n");
     try{
-      k5x5(5,0)
+      k5x5.solve(5,0);
     }
-    catch (IllegalArgumentException e){
+    catch (IllegalArgumentException e){ //out of bounds
+      System.out.println(e);
+    }
+    System.out.println("k5x5(-1, 2) --> IllegalArgumentException\n");
+    try{
+      k5x5.solve(-1,2);
+    }
+    catch (IllegalArgumentException e){ //negative
       System.out.println(e);
     }
   }
