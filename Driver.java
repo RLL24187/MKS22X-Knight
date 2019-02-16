28fa69d40f391373374771719c4a2c80bf9c1872
rleongPd5 <rleong00@stuy.edu>
2019-02-15 22:21:26 -0500

    Added test cases for solve

--- Driver.java
+++ Driver.java
@@ -1,6 +1,10 @@
 public class Driver{
   public static void main(String[] args){
-    KnightBoard k5x5 = new KnightBoard(5,5);
+    KnightBoard k2x4 = new KnightBoard(2, 4);
+    KnightBoard k6x6 = new KnightBoard(6, 6);
+    KnightBoard k5x5 = new KnightBoard(5, 5);
+    KnightBoard k7x7 = new KnightBoard(7, 7);
+    KnightBoard k6x8 = new KnightBoard(6, 8);
     System.out.println("-----------Testing KnightBoard constructor-----------\n");
     System.out.println("-----------Testing toString-----------\n");
     System.out.println("k5x5: \n");
@@ -24,7 +28,29 @@ public class Driver{
     }
 
     System.out.println("-----------Testing solve()-----------");
-    k5x5.solve(0,0);
+    System.out.println(k5x5.solve(0,0));
     System.out.println(k5x5.toString());
+    k5x5.revert();
+
+    System.out.println(k5x5.solve(3, 1));
+    System.out.println(k5x5.toString());
+    k5x5.revert();
+
+    System.out.println(k6x6.solve(4, 3));
+    System.out.println(k6x6.toString());
+    k6x6.revert();
+
+    System.out.println(k2x4.solve(0, 2));
+    System.out.println(k2x4.toString());
+    k2x4.revert();
+
+    System.out.println(k7x7.solve(5, 3));
+    System.out.println(k7x7.toString());
+    k7x7.revert();
+
+
+    System.out.println(k6x8.solve(2, 7));
+    System.out.println(k6x8.toString());
+    k5x5.revert();
   }
 }
